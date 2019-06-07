44e338f042e17bd699ecd735561962094e66a3ed
Edvan Junior <ed.jr@outlook.com.br>
2019-06-07 00:43:10 -0300

    todos os svgs foram colocados no projeto, animações tela de sessoes feitas

---
+++ js/sessoes.js
@@ -0,0 +1,17 @@
+
+function AlteraImagemNoHover(originClass,newImage) {  
+
+    $(originClass).hover(function() {
+    $('#imagem_bia').attr("src",newImage).hide().fadeIn();
+   
+    }, function(){
+    });
+
+}
+
+$(document).ready(function () {
+    AlteraImagemNoHover(".primeiro_card","../img/SVG/SESSOES/S1.svg");
+    AlteraImagemNoHover(".segundo_card","../img/SVG/SESSOES/S2.svg");
+    AlteraImagemNoHover(".terceiro_card","../img/SVG/SESSOES/S3.svg");
+});
+
