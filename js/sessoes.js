51e2f39aa32db9a3d9ae9aad1ae53138efafe19f
Edvan Junior <ed.jr@outlook.com.br>
2019-06-08 13:09:35 -0300

    imlementação de js relacionado ao menu

--- js/sessoes.js
+++ js/sessoes.js
@@ -1,4 +1,12 @@
 
+$(document).ready(function () {
+    abrirMenu();
+    AlteraImagemNoHover(".primeiro_card","../img/SVG/SESSOES/S1.svg");
+    AlteraImagemNoHover(".segundo_card","../img/SVG/SESSOES/S2.svg");
+    AlteraImagemNoHover(".terceiro_card","../img/SVG/SESSOES/S3.svg");
+});
+
+
 function AlteraImagemNoHover(originClass,newImage) {  
     $(originClass).hover(function() {
     $('#imagem_bia').attr("src",newImage);
@@ -6,9 +14,13 @@ function AlteraImagemNoHover(originClass,newImage) {
     });
 }
 
-$(document).ready(function () {
-    AlteraImagemNoHover(".primeiro_card","../img/SVG/SESSOES/S1.svg");
-    AlteraImagemNoHover(".segundo_card","../img/SVG/SESSOES/S2.svg");
-    AlteraImagemNoHover(".terceiro_card","../img/SVG/SESSOES/S3.svg");
-});
+
+function abrirMenu(){
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+        $('#sidebarCollapse').toggleClass('active');
+    });
+}
+
+
 
